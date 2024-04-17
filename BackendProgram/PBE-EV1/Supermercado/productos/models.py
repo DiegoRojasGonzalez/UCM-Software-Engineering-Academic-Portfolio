4e70fe0c119643a482a6fb30f77e7474c0c6cb5c
Diego Ignacio Rojas Gonzalez <gonzalez.rojas.ignacio.diego@gmail.com>
2024-04-16 20:25:46 -0400

    feat(style): :lipstick: add more styles in project

--- BackendProgram/PBE-EV1/Supermercado/productos/models.py
+++ BackendProgram/PBE-EV1/Supermercado/productos/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-
+# esquema de la tabla producto 
 class Producto(models.Model):
     codigoBarra = models.CharField(max_length=666)
     nombre = models.CharField(max_length=666)
--- BackendProgram/PBE-EV1/Supermercado/productos/views.py
+++ BackendProgram/PBE-EV1/Supermercado/productos/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Producto
 
+# con el orm obtenemos todos los objetos dentro de la tabla 'Producto'
+# tambien cargamos los productos al template
 def index(request):
-    productos = Producto.objects.all()  # Obtiene todos los productos de la base de datos
+    productos = Producto.objects.all() 
     return render(request, 'productos/index.html', {'productos': productos})
