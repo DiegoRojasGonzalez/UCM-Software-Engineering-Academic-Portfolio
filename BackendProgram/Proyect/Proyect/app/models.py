982c023c3a7e75d3b7eca6ccae1b3e64354b77aa
Diego Ignacio Rojas Gonzalez <gonzalez.rojas.ignacio.diego@gmail.com>
2024-06-02 21:41:21 -0400

    feat(db): :card_file_box: add models

--- BackendProgram/Proyect/Proyect/app/models.py
+++ BackendProgram/Proyect/Proyect/app/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Pelicula(models.Model):
+    pelicula_id = models.UUIDField(primary_key=True)
+    titulo = models.TextField()
+    genero = models.JSONField()
+    año_lanzamiento = models.IntegerField()
+    descripcion = models.TextField()
+    duracion = models.TextField()
+
+class Usuario(AbstractUser):
+    favoritas = models.JSONField(default=list)
+    lista_para_ver = models.JSONField(default=list)
+
+class CalificacionPelicula(models.Model):
+    rating_id = models.UUIDField(primary_key=True)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    pelicula = models.ForeignKey(Pelicula, on_delete=models.CASCADE)
+    valor_rating = models.IntegerField()
+    timestamp = models.DateTimeField()
+
+class ComentarioPelicula(models.Model):
+    comment_id = models.UUIDField(primary_key=True)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    pelicula = models.ForeignKey(Pelicula, on_delete=models.CASCADE)
+    texto_comentario = models.TextField()
+    timestamp = models.DateTimeField()
+
+class ReviewPelicula(models.Model):
+    review_id = models.UUIDField(primary_key=True)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    pelicula = models.ForeignKey(Pelicula, on_delete=models.CASCADE)
+    titulo_review = models.CharField(max_length=255)
+    texto_review = models.TextField()
+    valor_rating = models.IntegerField()
+    timestamp = models.DateTimeField()
