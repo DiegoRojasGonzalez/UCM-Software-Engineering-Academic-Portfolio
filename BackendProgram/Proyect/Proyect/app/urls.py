ff1c1bfb5f2610a24790b509a3e57659da87a6d9
Diego Ignacio Rojas Gonzalez <gonzalez.rojas.ignacio.diego@gmail.com>
2024-06-03 15:44:06 -0400

    feat(files): :sparkles: add auth services

--- BackendProgram/Proyect/Proyect/app/urls.py
+++ BackendProgram/Proyect/Proyect/app/urls.py
@@ -3,6 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('login/', login, name="login"),
+    path('logout/', logout, name="logout"),
     path('register/', register, name="register"),
     path('home/', loadIndex, name='index'),                                     # GET
     path('addMovie/', addMovie, name='addMovie'),                               # POST 
--- BackendProgram/Proyect/Proyect/app/views.py
+++ BackendProgram/Proyect/Proyect/app/views.py
@@ -1,3 +1,8 @@
+
+from django.contrib.auth import authenticate,logout as auth_logout , login as auth_login
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required 
+
 from django.shortcuts import redirect, render
 from django.http import JsonResponse, HttpResponseForbidden
 from .models import Pelicula
@@ -5,17 +10,55 @@ from bson import ObjectId
 
 # Create your views here.
 
-# Index View 
+
 def login(request):
+    if request.method == 'POST':
+        print('hola estas haciendo post ')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            return redirect('index')
+        else:
+            return JsonResponse({'success': False, 'message': 'Nombre de usuario o contraseña incorrectos.'})
+    
     return render(request, 'login.html')
 
+def logout(request):
+    auth_logout(request)
+    return redirect('login')
+
 def register(request):
+    if request.method == 'POST':
+        print('se esta haciendo un post ')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        password_confirm = request.POST.get('password_confirm')
+
+        if password == password_confirm:
+            if User.objects.filter(username=username).exists():
+                return JsonResponse({'success': False, 'message': 'El nombre de usuario ya existe.'})
+            elif User.objects.filter(email=email).exists():
+                return JsonResponse({'success': False, 'message': 'El correo electrónico ya está registrado.'})
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+                auth_login(request, user)
+                return redirect('index')
+        else:
+            return JsonResponse({'success': False, 'message': 'Las contraseñas no coinciden.'})
+
     return render(request, 'register.html')
 
+@login_required
 def loadIndex(request):
     peliculas = Pelicula.objects.all()
     return render(request, 'index.html', {'peliculas': peliculas})
 
+@login_required
 def addMovie(request):
     if request.method == 'POST':
         titulo = request.POST.get('titulo')
@@ -37,6 +80,7 @@ def addMovie(request):
 
     return JsonResponse({'error': 'Se esperaba una solicitud POST'})
 
+@login_required
 def deleteMovie(request, pelicula_id):
     if request.method == 'POST':
         pelicula_id_object = ObjectId(pelicula_id)
@@ -45,7 +89,8 @@ def deleteMovie(request, pelicula_id):
         return redirect('index')
     else:
         return HttpResponseForbidden()
-    
+
+@login_required
 def updateMovie(request, pelicula_id):
     if request.method == 'POST':
         # Obtener la película existente
