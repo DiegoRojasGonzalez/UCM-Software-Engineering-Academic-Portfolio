81612044d0d758935cf907fac1c01840a0cd3866
Diego <gonzalez.rojas.ignacio.diego@gmail.com>
2024-06-16 14:17:32 -0400

    feat(files): :sparkles: add reports and new transaccions

---
+++ UnstructuredDatabases/Proyect/README.md
@@ -0,0 +1,6 @@
+# Install
+
+-   up docker with `docker compose up -d`
+-   go to api folder whit `cd api/`
+-   run `npm i `
+-   run `npm run dev `
--- UnstructuredDatabases/Proyect/api/index.js
+++ UnstructuredDatabases/Proyect/api/index.js
@@ -10,7 +10,7 @@ mongoose
     .then(() => console.log("Conectado a MongoDB"))
     .catch((err) => console.log("Error al conectar a MongoDB:", err));
 
-// Definir un esquema y modelo para los ítems
+//? ESQUEMA
 const itemSchema = new mongoose.Schema({
     tittle: String,
     anno: Number,
@@ -25,9 +25,10 @@ const Item = mongoose.model("Item", itemSchema);
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("on");
+    res.send("200");
 });
 
+//! GET ALL ITEMS
 app.get("/items", async (req, res) => {
     try {
         const items = await Item.find();
@@ -37,6 +38,102 @@ app.get("/items", async (req, res) => {
     }
 });
 
+//! REPORTES
+
+//? REPORTE 1 - Reporte de Películas por Año de Estreno:
+app.get("/reporte1", async (req, res) => {
+    try {
+        const moviesByYear = await Item.aggregate([
+            {
+                $group: {
+                    _id: "$anno",
+                    movies: { $push: "$$ROOT" },
+                },
+            },
+            {
+                $sort: { _id: 1 },
+            },
+        ]);
+        res.json(moviesByYear);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+//? REPORTE 2 - Reporte de Películas por Género:
+app.get("/reporte2", async (req, res) => {
+    try {
+        const moviesByGenre = await Item.aggregate([
+            {
+                $group: {
+                    _id: "$genere",
+                    movies: { $push: "$$ROOT" },
+                },
+            },
+        ]);
+        res.json(moviesByGenre);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+//? REPORTE 3 - Reporte de Películas con Mayor Puntuación:
+app.get("/reporte3", async (req, res) => {
+    try {
+        const moviesByPoints = await Item.aggregate([
+            {
+                $sort: { points: -1 },
+            },
+            {
+                $limit: 10, // Obtener las 10 películas con mayor puntuación
+            },
+        ]);
+        res.json(moviesByPoints);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+//? REPORTE 4 - Reporte de Películas por Director:
+app.get("/reporte4", async (req, res) => {
+    try {
+        const moviesByDirector = await Item.aggregate([
+            {
+                $group: {
+                    _id: "$director",
+                    movies: { $push: "$$ROOT" },
+                },
+            },
+        ]);
+        res.json(moviesByDirector);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+//? REPORTE 5 - Reporte de Películas por Duración:
+app.get("/reporte5", async (req, res) => {
+    try {
+        const moviesByDuration = await Item.aggregate([
+            {
+                $group: {
+                    _id: "$time_minuts",
+                    movies: { $push: "$$ROOT" },
+                },
+            },
+            {
+                $sort: { _id: 1 },
+            },
+        ]);
+        res.json(moviesByDuration);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+//! TRANSACCIONES
+
+//? Agregar peliculas
 app.post("/items", async (req, res) => {
     const newItem = new Item({
         tittle: req.body.tittle,
@@ -51,6 +148,32 @@ app.post("/items", async (req, res) => {
         res.status(201).json(savedItem);
     } catch (err) {
         res.status(400).send(err);
+    }
+});
+
+//? Eliminar todas las películas de un género específico:
+app.post("/eliminar-peliculas", async (req, res) => {
+    const { genero } = req.body;
+    try {
+        await Item.deleteMany({ genere: genero });
+        res.send(
+            `Todas las películas del género ${genero} han sido eliminadas.`
+        );
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+//? Cambiar el género de todas las películas según el año:
+app.put("/cambiar-genero", async (req, res) => {
+    const { anno, nuevoGenero } = req.body;
+    try {
+        await Item.updateMany({ anno: anno }, { genere: nuevoGenero });
+        res.send(
+            `Se ha cambiado el género de todas las películas del año ${anno} a ${nuevoGenero}.`
+        );
+    } catch (err) {
+        res.status(500).send(err);
     }
 });
 
