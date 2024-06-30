35f6a4c275371108214a50c054f4918112f026a1
Diego <gonzalez.rojas.ignacio.diego@gmail.com>
2024-06-30 16:10:28 -0400

    feat: :memo: add docs for urls.

--- BackendProgram/Evaluation3/project/project/urls.py
+++ BackendProgram/Evaluation3/project/project/urls.py
@@ -7,8 +7,8 @@ from rest_framework_simplejwt.views import ( TokenObtainPairView, TokenRefreshVi
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'), # [ obtain refresh and token ]
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), # [ refresh token ]
 
     # Customer   
     path('customers/', customers),# ['GET']
