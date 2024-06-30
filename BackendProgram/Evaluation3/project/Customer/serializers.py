784fcc810e4476d4b77c91603d9da1dc5d661ad1
Diego <gonzalez.rojas.ignacio.diego@gmail.com>
2024-06-30 15:25:10 -0400

    feat: :sparkles: all operations added
    crud customers and address for customers complete

--- BackendProgram/Evaluation3/project/Customer/serializers.py
+++ BackendProgram/Evaluation3/project/Customer/serializers.py
@@ -1,7 +1,39 @@
 from rest_framework import serializers
-from .models import Customer
+from .models import Customer , Address
 
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
-        fields = '__all__'
+        fields = ['id', 'first_name', 'last_name', 'email', 'phone_number']
+
+    def create(self, validated_data):
+        customer = Customer.objects.create(**validated_data)
+        return customer
+    
+    def update(self, instance, validated_data):
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.email = validated_data.get('email', instance.email)
+        instance.phone_number = validated_data.get('phone_number', instance.phone_number)
+        instance.save()
+        return instance
+
+
+class AddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+        fields = ['id', 'customer_id', 'address_line1', 'address_line2', 'city', 'state', 'postal_code', 'country']
+
+    def create(self, validated_data):
+        address = Address.objects.create(**validated_data)
+        return address
+
+    def update(self, instance, validated_data):
+        instance.address_line1 = validated_data.get('address_line1', instance.address_line1)
+        instance.address_line2 = validated_data.get('address_line2', instance.address_line2)
+        instance.city = validated_data.get('city', instance.city)
+        instance.state = validated_data.get('state', instance.state)
+        instance.postal_code = validated_data.get('postal_code', instance.postal_code)
+        instance.country = validated_data.get('country', instance.country)
+        instance.save()
+        return instance
--- BackendProgram/Evaluation3/project/Customer/views.py
+++ BackendProgram/Evaluation3/project/Customer/views.py
@@ -6,10 +6,89 @@ from rest_framework import status
 from rest_framework.views import APIView
 
 from Customer.models import Customer, Address
-from Customer.serializers import CustomerSerializer
+from Customer.serializers import CustomerSerializer,  AddressSerializer
 
 @api_view(['GET'])
-def get_customers(request):
+def customers(request):
     customers = Customer.objects.all()
     serializer = CustomerSerializer(customers, many=True)
     return Response(serializer.data)
+
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+def customer(request, id=0):
+    if request.method == 'GET':
+        try:
+            customer = Customer.objects.get(pk=id)
+        except Customer.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = CustomerSerializer(customer)
+        return Response(serializer.data)
+    
+    if request.method == 'POST':
+        serializer = CustomerSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    if request.method == 'PUT':
+        try:
+            customer = Customer.objects.get(pk=id)
+        except Customer.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = CustomerSerializer(customer, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    if request.method == 'DELETE':
+        try:
+            customer = Customer.objects.get(pk=id)
+        except Customer.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+        customer.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+def address(request, id=0):
+
+    if request.method == 'GET':
+        addresses = Address.objects.filter(customer_id=id)
+        serializer = AddressSerializer(addresses, many=True)
+        return Response(serializer.data)
+    
+    if request.method == 'POST':
+        serializer = AddressSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    if request.method == 'PUT':
+        try:
+            address = Address.objects.get(pk=id)
+        except Address.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = AddressSerializer(address, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    if request.method == 'DELETE':
+        try:
+            address = Address.objects.get(pk=id)
+        except Address.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+        address.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
