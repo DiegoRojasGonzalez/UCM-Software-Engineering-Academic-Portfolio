0579e9a61755b4eb04259df69ef94e8b21d03f6a
Diego <gonzalez.rojas.ignacio.diego@gmail.com>
2024-06-30 12:48:06 -0400

    feat: :sparkles: add database conection config

--- BackendProgram/Evaluation3/project/Customer/models.py
+++ BackendProgram/Evaluation3/project/Customer/models.py
@@ -2,21 +2,17 @@ from django.db import models
 
 # Create your models here.
 
-from django.db import models
-
-# Create your models here.
-
 class Customer(models.Model):
-first_name = models.CharField(max_length=99)
-last_name = models.CharField(max_length=99)
-email = models.CharField(max_length=20)
-phone_number = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=99)
+    last_name = models.CharField(max_length=99)
+    email = models.CharField(max_length=20)
+    phone_number = models.CharField(max_length=20)
 
 class Address(models.Model):
-customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
-address_line1 = models.CharField(max_length=199)
-address_line2 = models.CharField(max_length=199)
-city = models.CharField(max_length=99)
-state = models.CharField(max_length=99)
-postal_code = models.CharField(max_length=20)
-country = models.CharField(max_length=99)
+    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    address_line1 = models.CharField(max_length=199)
+    address_line2 = models.CharField(max_length=199)
+    city = models.CharField(max_length=99)
+    state = models.CharField(max_length=99)
+    postal_code = models.CharField(max_length=20)
+    country = models.CharField(max_length=99)
--- BackendProgram/Evaluation3/project/project/settings.py
+++ BackendProgram/Evaluation3/project/project/settings.py
@@ -74,10 +74,17 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydatabase',
+        'USER': 'root',
+        'PASSWORD': 'rootpassword',
+        'HOST': '172.19.0.2',
+        'PORT': '3306',
     }
 }
 
