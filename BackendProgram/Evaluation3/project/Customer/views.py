0c6063220cc277c7d2e03ebaf0285eaf1bcedb73
Diego <gonzalez.rojas.ignacio.diego@gmail.com>
2024-06-30 13:48:42 -0400

    feat: :sparkles: get constumers

--- BackendProgram/Evaluation3/project/Customer/views.py
+++ BackendProgram/Evaluation3/project/Customer/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+from rest_framework.views import APIView
 
-# Create your views here.
+from Customer.models import Customer, Address
+from Customer.serializers import CustomerSerializer
+
+@api_view(['GET'])
+def get_customers(request):
+    customers = Customer.objects.all()
+    serializer = CustomerSerializer(customers, many=True)
+    return Response(serializer.data)
