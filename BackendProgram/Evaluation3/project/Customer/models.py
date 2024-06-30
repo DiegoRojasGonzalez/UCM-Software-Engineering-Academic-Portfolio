c76397be90b3ba43e90d4489984dbedae46273b9
Diego <gonzalez.rojas.ignacio.diego@gmail.com>
2024-06-30 13:56:44 -0400

    feat: :sparkles: add selft str metods} on model
    }

--- BackendProgram/Evaluation3/project/Customer/models.py
+++ BackendProgram/Evaluation3/project/Customer/models.py
@@ -8,6 +8,9 @@ class Customer(models.Model):
     email = models.CharField(max_length=20)
     phone_number = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.first_name 
+
 class Address(models.Model):
     customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
     address_line1 = models.CharField(max_length=199)
@@ -16,3 +19,6 @@ class Address(models.Model):
     state = models.CharField(max_length=99)
     postal_code = models.CharField(max_length=20)
     country = models.CharField(max_length=99)
+
+    def __str__(self):
+        return self.address_line1 + " - " + self.city + " | " + self.customer_id.first_name 
